05ba7e73953511f8904cd0afddc7c34c63e49e9b
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-08-21 13:59:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+//Multiplying
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+//Random
+const random = Math.ceil(Math.random() * 5);
+
+//Modulus
+const num3 = 24;
+const num4 = 5;
+let mod = num3%num4;
+
+//Max
+const max = Math.max(5, 20, 13, 16, 8, 7);
